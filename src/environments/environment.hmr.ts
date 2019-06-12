21d94dd7766a590b4c7fc6672bb8c1faed38c4b5
vaishakh <vaishakh.cherukat@rapidvaluesolutions.com>
2019-06-12 14:56:13 +0530

    angular firebase setup

---
+++ src/environments/environment.hmr.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    hmr: true,
+    firebase: {
+        apiKey: "AIzaSyC9GFrnXAyd-5_u2nhe0Df43vcnQnaFfD0",
+        authDomain: "my-awesome-project-4ec4b.firebaseapp.com",
+        databaseURL: "https://my-awesome-project-4ec4b.firebaseio.com",
+        projectId: "my-awesome-project-4ec4b",
+        storageBucket: "my-awesome-project-4ec4b.appspot.com",
+        messagingSenderId: "340508966157",
+        appId: "1:340508966157:web:5300443bf72f30fa"
+    }
+};
